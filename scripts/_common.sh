fa508cceceadd5e5d05f1fbd2801c39346e252fa
Mayeul Cantan <oss@mayeul.net>
2022-08-31 15:54:02 +0200

    Try to upgrade signald

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="postgresql python3 python3-dev build-essential libolm-dev"
-extra_dependencies="libunixsocket-java signald signaldctl"
+extra_dependencies="libunixsocket-java signald=0.21.1 signaldctl"
 #=================================================
 # PERSONAL HELPERS
 #=================================================
